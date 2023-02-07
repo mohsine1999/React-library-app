94e71af96d17d0c868835f69ee4a9363bbe9251d
mohsine1999 <mohsine090hajjar@gmail.com>
2023-02-07 16:02:38 +0100

    FrontEnd

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { HomePage } from "./layouts/HomePage/HomePage";
+export const  App = () => {
+  return (
+    <div>
+      <Navbar />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
