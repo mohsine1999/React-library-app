bfd14b5f7afbd28d5d29e23d2ff57b13f070006a
mohsine1999 <mohsine090hajjar@gmail.com>
2023-02-09 17:16:02 +0100

    Consume the Rest API

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -1,8 +1,65 @@
 import { ReturnBook } from "./ReturnBook";
+import { useState, useEffect } from "react";
+import BookModel from "../../../models/BookModel";
+
 
 export const Carousel = () => {
 
+  const [books, setBooks] = useState<BookModel[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const baseUrl: string = "http://localhost:8080/api/books"
+
+      const url: string = `${baseUrl}?page=0&size=9`;
+
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error('Somethinf went wrong!');
+      }
+
+      const responseJson = await response.json();
+
+      const responseData = responseJson._embedded.books;
+
+      const loadedBooks: BookModel[] = [];
+
+      for (const key in responseData) {
+        loadedBooks.push({
+          id: responseData[key].id,
+          title: responseData[key].title,
+          author: responseData[key].author,
+          description: responseData[key].description,
+          copies: responseData[key].copies,
+          copiesAvailable: responseData[key].copiesAvailable,
+          category: responseData[key].category,
+          img: responseData[key].img
+        })
+      }
+      setBooks(loadedBooks);
+      setIsLoading(false);
+    };
+    fetchBooks().catch((error: any) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    })
+  }, []);
+
+  if(isLoading){
+    return (
+      <div className="container m-5">
+        <p>Loading...</p>
+      </div>
+    )
+  }
+  if (httpError){
+    <div className="container m-5">
+      <p>{httpError}</p>
+    </div>
+  }
 
   return (
     <div className='container mt-5' style={{ height: 550 }}>
@@ -10,7 +67,7 @@ export const Carousel = () => {
         <h3>Find your next "I stayed up too late reading." book.</h3>
       </div>
       <div id='carouselExampleControls' className='carousel carousel-dark slide mt-5 
-                d-none d-lg-block' data-bs-interval='false'>
+              d-none d-lg-block' data-bs-interval='false'>
 
         {/* Desktop */}
         <div className='carousel-inner'>
