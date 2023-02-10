066ffd25cb1cb0ae068fe43169ecb710efec68d4
mohsine1999 <mohsine090hajjar@gmail.com>
2023-02-10 17:22:57 +0100

    new version with Pagination component

--- src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -1,117 +1,135 @@
 import { useState, useEffect } from "react";
 import BookModel from "../../models/BookModel";
+import { Pagination } from "../Utils/Pagination";
 import { SprinnerLoading } from "../Utils/SpinnerLoading";
 import { SearchBook } from "./components/SearchBook";
 
 export const SearchBooksPage = () => {
-        
-  const [books, setBooks] = useState<BookModel[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [httpError, setHttpError] = useState(null);
-  
-  
-  useEffect(() => {
-       const fetchBooks = async () => {
+
+    const [books, setBooks] = useState<BookModel[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [booksPerPage] = useState(5);
+    const [totalAmountOfBooks, settotalAmountOfBooks] = useState(0);
+    const [totalPages, setTotalPages] = useState(0);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
             const baseUrl: string = "http://localhost:8080/api/books"
-                
-             const url: string = `${baseUrl}?page=0&size=5`;
-                
-             const response = await fetch(url);
-                
-        if (!response.ok) {
-            throw new Error('Somethinf went wrong!');
-        }
-                
-             const responseJson = await response.json();
-                
-             const responseData = responseJson._embedded.books;
-                
-             const loadedBooks: BookModel[] = [];
-                
-             for (const key in responseData) {
-                    loadedBooks.push({
-                      id: responseData[key].id,
-                      title: responseData[key].title,
-                      author: responseData[key].author,
-                      description: responseData[key].description,
-                      copies: responseData[key].copies,
-                      copiesAvailable: responseData[key].copiesAvailable,
-                      category: responseData[key].category,
-                      img: responseData[key].img
-                    })
-                      }
-             setBooks(loadedBooks);
-             setIsLoading(false);
-             };
-             fetchBooks().catch((error: any) => {
-             setIsLoading(false);
-             setHttpError(error.message);
-             })}, []);
 
-if(isLoading){
-       return (
-         <SprinnerLoading />
-       )
-       }
-if (httpError){
-       <div className="container m-5">
-           <p>{httpError}</p>
-       </div>
-   }
+            const url: string = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
 
-return (
-    <div>
-        <div className="container" >
-            <div>
-                <div className="row mt-5">
-                    <div className="col-6">
-                        <div className="d-flex">
-                            <input className="form-control me-2" type="search"
-                                placeholder="Search" aria-labelledby="Search" />
-                            <button className="btn btn-outline-success">
-                               Search         
-                            </button>                                
-                        </div>                
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error('Somethinf went wrong!');
+            }
+
+            const responseJson = await response.json();
+
+            const responseData = responseJson._embedded.books;
+
+            settotalAmountOfBooks(responseJson.page.totalElements);
+            setTotalPages(responseJson.page.totalPages);
+
+            const loadedBooks: BookModel[] = [];
+
+            for (const key in responseData) {
+                loadedBooks.push({
+                    id: responseData[key].id,
+                    title: responseData[key].title,
+                    author: responseData[key].author,
+                    description: responseData[key].description,
+                    copies: responseData[key].copies,
+                    copiesAvailable: responseData[key].copiesAvailable,
+                    category: responseData[key].category,
+                    img: responseData[key].img
+                })
+            }
+            setBooks(loadedBooks);
+            setIsLoading(false);
+        };
+        fetchBooks().catch((error: any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        })
+        window.scrollTo(0,0);
+    }, [currentPage]);
+
+    if (isLoading) {
+        return (
+            <SprinnerLoading />
+        )
+    }
+    if (httpError) {
+        <div className="container m-5">
+            <p>{httpError}</p>
+        </div>
+    }
+    const indexOfLastBook: number = currentPage * booksPerPage;
+    const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
+    let lastItem = booksPerPage * currentPage <= totalAmountOfBooks ?
+        booksPerPage * currentPage : totalAmountOfBooks;
+
+    const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
+
+    return (
+        <div>
+            <div className="container" >
+                <div>
+                    <div className="row mt-5">
+                        <div className="col-6">
+                            <div className="d-flex">
+                                <input className="form-control me-2" type="search"
+                                    placeholder="Search" aria-labelledby="Search" />
+                                <button className="btn btn-outline-success">
+                                    Search
+                                </button>
+                            </div>
+                        </div>
+                        <div className="clo-4">
+                            <div className="dropdown">
+                                <button className="btn btn-secondary dropdown-toggle" type="button"
+                                    id="dropdownMenuButton1" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    Category
+                                </button>
+                                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                    <li>
+                                        <a className="dropdown-item" href="#">All</a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item" href="#">FrontEnd</a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item" href="#">Back End</a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item" href="#">Data</a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item" href="#">DevOps</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                    <div className="clo-4">
-                       <div className="dropdown">
-                           <button className="btn btn-secondary dropdown-toggle" type="button"
-                               id="dropdownMenuButton1" data-bs-toggle="dropdown"
-                               aria-expanded="false">
-                               Category
-                           </button>
-                           <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                               <li>
-                                   <a className="dropdown-item" href="#">All</a>     
-                               </li>
-                               <li>
-                                   <a className="dropdown-item" href="#">FrontEnd</a>     
-                               </li>
-                               <li>
-                                   <a className="dropdown-item" href="#">Back End</a>     
-                               </li>
-                               <li>
-                                   <a className="dropdown-item" href="#">Data</a>     
-                               </li>
-                               <li>
-                                   <a className="dropdown-item" href="#">DevOps</a>     
-                               </li>         
-                           </ul>             
-                       </div>                   
-                    </div>
-                </div>
                     <div className="mt-3">
-                        <h5> Number of results : (22)</h5>
+                        <h5> Number of results : ({totalAmountOfBooks})</h5>
                     </div>
                     <p>
-                        1 to 5 of 22 items :
+                        {indexOfFirstBook + 1} to {lastItem} of {totalAmountOfBooks} items :
                     </p>
-                       {books.map(book => (
-                       <SearchBook book = {book} key = {book.id} />
-                    ))}    
+                    {books.map(book => (
+                        <SearchBook book={book} key={book.id} />
+                    ))}
+                    {totalPages > 1 &&
+                        <Pagination currentPage={currentPage} totalPages={totalPages} paginate={paginate} />
+                    }
+                </div>
             </div>
         </div>
-    </div>                
-);
+    );
 
 }
