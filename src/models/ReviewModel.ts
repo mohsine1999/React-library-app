8303c1282b71bbb18a3f62f99db43b9efe4717d2
mohsine1999 <mohsine090hajjar@gmail.com>
2023-03-01 20:35:12 +0100

    reviewModel updating

--- src/models/ReviewModel.ts
+++ src/models/ReviewModel.ts
@@ -5,7 +5,7 @@ class ReviewModel {
     rating: number;
     book_id: number;
     reviewDescription?: string;
-//test
+//test O1
     constructor(id: number, userEmail: string, date: string, 
         rating: number, book_id: number, reviewDescription: string) {
             
